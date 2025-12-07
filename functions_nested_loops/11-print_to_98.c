93714dbc695f3c07cb4920526e5044af4965ea0f
RahimShikhgayibov <rehim1718@gmail.com>
2025-12-07 18:38:15 +0000

    new changes

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,53 +1,58 @@
 #include "main.h"
 
 /**
+ * print_num - prints an integer using _putchar
+ * @n: integer to print
+ */
+static void print_num(int n)
+{
+	unsigned int num;
+	int i = 0;
+	char buf[12];
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = (unsigned int)(-n);
+	}
+	else
+		num = (unsigned int)n;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (num > 0)
+	{
+		buf[i++] = '0' + (num % 10);
+		num /= 10;
+	}
+
+	while (i-- > 0)
+		_putchar(buf[i]);
+}
+
+/**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: starting number
- *
- * Numbers are separated by a comma followed by a space, and the sequence
- * ends with a newline.
  */
 void print_to_98(int n)
 {
 	int curr = n;
-	unsigned int abs_val;
-	int i;
-	char buf[12]; /* enough for int digits + sign */
 
 	while (curr != 98)
 	{
-		/* print curr */
-		if (curr < 0)
-		{
-			_putchar('-');
-			abs_val = (unsigned int)(-curr);
-		}
-		else
-			abs_val = (unsigned int)curr;
-
-		/* build digits in reverse order */
-		i = 0;
-		do {
-			buf[i++] = '0' + (abs_val % 10);
-			abs_val /= 10;
-		} while (abs_val > 0);
-
-		/* output digits in correct order */
-		while (i-- > 0)
-			_putchar(buf[i]);
-
+		print_num(curr);
 		_putchar(',');
 		_putchar(' ');
-
-		/* move toward 98 */
 		if (n < 98)
 			curr++;
 		else
 			curr--;
 	}
 
-	/* print 98 and newline */
-	_putchar('9');
-	_putchar('8');
+	print_num(98);
 	_putchar('\n');
 }
