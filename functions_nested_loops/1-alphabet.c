c8a1c0e36634ab2458d53cef11e08459021f7a04
RahimShikhgayibov <rehim1718@gmail.com>
2025-12-07 18:18:23 +0000

    h

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet - prints the alphabet.
- *
- *
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
  */
 void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a' ; c <= 'z' ; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
+
 	_putchar('\n');
 }
