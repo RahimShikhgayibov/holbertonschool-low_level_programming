c7b27e0436e7d55b3c09212ca8c2fb8cc1b36ade
RahimShikhgayibov <rehim1718@gmail.com>
2025-12-14 16:36:30 +0000

    Pointers tasks

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string
+ *
+ * Return: the integer value of the string
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, num_started = 0;
+	int result = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			; /* do nothing */
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num_started = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (num_started)
+			break; /* stop if number has started and non-digit found */
+		i++;
+	}
+
+	return (result * sign);
+}
