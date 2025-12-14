cf869855265dfa8cfd0f24f7c8c7a4f7ec0bc505
RahimShikhgayibov <rehim1718@gmail.com>
2025-12-14 16:38:00 +0000

    Pointers tasks

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -20,6 +21,16 @@ int _atoi(char *s)
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			num_started = 1;
+
+			/* Check for overflow before multiplying */
+			if (result > (INT_MAX - (s[i] - '0')) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
 			result = result * 10 + (s[i] - '0');
 		}
 		else if (num_started)
