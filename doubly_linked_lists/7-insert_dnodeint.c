27e46dd1bc3094bc01687df115cbfc54486defc3
Rahim <rehim1718@gmail.com>
2026-01-18 19:35:48 +0000

    s

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,23 +15,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	unsigned int i;
 
-	/* Case 1: Insert at the beginning */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	/* Traverse to the node *before* the insertion point */
 	for (i = 0; temp != NULL && i < idx - 1; i++)
 		temp = temp->next;
 
-	/* Case: Index is out of range */
 	if (temp == NULL)
 		return (NULL);
 
-	/* Case 2: Insert at the end */
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	/* Case 3: Insert in the middle */
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -39,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->prev = temp;
 	new->next = temp->next;
-	
+
 	temp->next->prev = new;
 	temp->next = new;
 
